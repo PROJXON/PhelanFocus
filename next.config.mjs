f6a5b6f596c4895f884e20d39577a3e2f438df84
BastThePM <152586621+TheRealBastioul@users.noreply.github.com>
2025-02-08 18:00:11 -0500

    resetting to original state

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: '/PhelanFocus', // Your GitHub repo name
-    assetPrefix: '/PhelanFocus', // Same as above
-    images: {
-      unoptimized: true, // Avoid issues with Next.js image optimization
-    },
-    output: 'export', // Add this line for static export
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+export default nextConfig;
