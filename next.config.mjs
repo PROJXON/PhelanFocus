d45fa3330a6b7bfb5fadb1069347b7ceb9423000
BastThePM <152586621+TheRealBastioul@users.noreply.github.com>
2025-02-10 09:12:05 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: "/PhelanFocus",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
   };
