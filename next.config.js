e547b15548831d2a65abcfff1f3c24795a511996
BastThePM <152586621+TheRealBastioul@users.noreply.github.com>
2025-02-10 09:14:13 -0500

    fixed next.config

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/PhelanFocus",
+  output: "export",  // Enables static exports
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
