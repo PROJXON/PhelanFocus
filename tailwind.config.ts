0a981827feb37252c4a55dd5f4b94fa96827aa6a
davenpos <davenpos@uwindsor.ca>
2025-07-28 18:30:03 -0400

    Installed TypeScript and converted tailwind.config.mjs to TS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -16,3 +17,5 @@ export default {
   },
   plugins: [],
 };
+
+export default config;
